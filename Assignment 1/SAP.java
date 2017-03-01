045e7f8b695274c4c9c4f720f1ce6cdec89542a5
Albert Jiang <albert594250@gmail.com>
2017-03-01 23:25:24 +0000

    corrected API

---
+++ Assignment 1/SAP.java
@@ -0,0 +1,27 @@
+import edu.princeton.cs.algs4.Digraph;
+
+public class SAP {
+	Digraph net;
+
+	// constructor takes a rooted DAG as argument
+	public SAP(Digraph G) {
+		net = new Digraph(G);
+	}
+
+	// length of shortest ancestral path between v and w
+	public int length(int v, int w) {
+
+	}
+
+	// a shortest common ancestor of vertices v and w
+	public int ancestor(int v, int w)
+
+	// length of shortest ancestral path of vertex subsets A and B
+	public int length(Iterable<Integer> subsetA, Iterable<Integer> subsetB)
+
+	// a shortest common ancestor of vertex subsets A and B
+	public int ancestor(Iterable<Integer> subsetA, Iterable<Integer> subsetB)
+
+	// do unit testing of this class
+	public static void main(String[] args)
+}
--- Assignment 1/WordNet.java
+++ Assignment 1/WordNet.java
@@ -2,6 +2,7 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.RedBlackBST;
+import edu.princeton.cs.algs4.Queue;
 
 import java.util.ArrayList;
 
@@ -9,6 +10,7 @@ public class WordNet {
 	Digraph net;
 	RedBlackBST<Integer, String> idSynMap = new RedBlackBST<Integer, String>();
 	RedBlackBST<String, String> synGloMap = new RedBlackBST<String, String>();
+	RedBlackBST<String, Integer> synIdMap = new RedBlackBST<String, Integer>();
 
 	// constructor takes the name of the two input files
 	public WordNet(String synsets, String hypernyms) {
@@ -26,6 +28,7 @@ public class WordNet {
 			for (String noun : lineElements[1].split(" ")) {
 				if (!synGloMap.contains(noun)) {
 					synGloMap.put(noun, lineElements[2]);
+					synIdMap.put( noun, Integer.parseInt(lineElements[0]) );
 					System.out.println(noun);
 					System.out.println(lineElements[2]);
 				}
@@ -46,28 +49,39 @@ public class WordNet {
 	}
 
 	// all WordNet nouns
-	// public Iterable<String> nouns() {
-
-	//}
+	public Iterable<String> nouns() {
+		return synGloMap.keys();
+	}
 
 	// is the word a WordNet noun?
 	public boolean isNoun(String word) { return synGloMap.contains(word); }
 
 	// a synset (second field of synsets.txt) that is a shortest common ancestor
 	// of noun1 and noun2 (defined below)
-	//public String sca(String noun1, String noun2) {
-
+	//public String sap(String noun1, String noun2) {
+	//	SAP sap = new SAP(this.net);
+	//	return this.idSynMap.get(SAP.ancestor(this.synIdMap.get(noun1),
+	//															 this.synIdMap.get(noun2)));
 	//}
 
 	// distance between noun1 and noun2 (defined below)
 	//public int distance(String noun1, String noun2) {
-
+	//	SAP sca = new SAP(this.net);
+	//	return SAP.length(this.synIdMap.get(noun1), this.synIdMap.get(noun2));
 	//}
 
 	// do unit testing of this class
-
 	public static void main(String[] args) {
 		WordNet myNet = new WordNet("synsets.txt", "hypernyms.txt");
 		System.out.println(myNet.isNoun("Aegina"));
+		int count = 0;
+		for (String noun : myNet.nouns()) {
+			if (count <= 10) {
+				System.out.println(noun);
+				count++;
+			}
+			else break;
+
+		}
 	}
 }
